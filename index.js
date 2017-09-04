2839069735084079a8b32d823114fb08070213fa
Nathan Stanford Sr <nathan.stanford@gmail.com>
2017-09-04 14:14:38 -0400

    beta version with a second language.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = function() {
-    console.log('Hello World!');
+module.exports = function(lang) {
+    if(lang === 'es'){
+        console.log('Hola Mundo!');
+    } else {
+        console.log('Hello World!');
+    }
 }
